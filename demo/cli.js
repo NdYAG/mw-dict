382ef7898856fc690a3b9fa0d6d5460e0440661f
Ximing <netizendxm@gmail.com>
2017-09-05 16:51:45 +0800

    rename demo

---
+++ demo/cli.js
@@ -0,0 +1,23 @@
+/*
+  Exmaples:
+  node demo/cli.js path
+  node demo/cli.js 'glide path'
+ */
+
+const { inspect } = require('util')
+const { CollegiateDictionary } = require('../')
+const API_KEY = require('./config')
+
+const dict = new CollegiateDictionary((key = API_KEY))
+
+let argv = process.argv
+let word = argv[argv.length - 1]
+
+dict
+  .lookup(word)
+  .then(result => {
+    console.log(inspect(result, false, null))
+  })
+  .catch(err => {
+    console.log(err)
+  })
